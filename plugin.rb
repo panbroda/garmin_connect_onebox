3929ac1aec324c7b2c695176d7e7383e64e159d7
Ewan McDougall <ewan@mrloop.com>
2015-06-16 17:40:40 +0100

    Use https embed url

--- plugin.rb
+++ plugin.rb
@@ -17,7 +17,7 @@ module Onebox
       end
 
       def to_html
-        "<iframe width='600' height='600' frameborder='0' src='http://connect.garmin.com/course/embed/#{id}'></iframe>"
+        "<iframe width='600' height='600' frameborder='0' src='https://connect.garmin.com/course/embed/#{id}'></iframe>"
       end
     end
   end
