0dc5a490dc169c1625a7d75cb9b019bc4959a6f0
mrloop <ewan@mrloop.com>
2016-07-29 20:53:05 +0100

    Test #to_html

--- plugin_test.rb
+++ plugin_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 module Onebox
   module Engine
     class GarminConnectOnebox
+      attr_accessor :url
       def self.matches_regexp regex
         "stub"
       end
@@ -29,5 +30,20 @@ class TestRegex < Minitest::Test
     assert_equal "123456", "https://connect.garmin.com/modern/course/123456".match(@regex)[1]
     assert_equal "123456", "https://connect.garmin.com/modern/activity/embed/123456".match(@regex)[1]
   end
+end
 
+class TestGarminConnectOnebox < Minitest::Test
+  def setup
+    @box = Onebox::Engine::GarminConnectOnebox.new
+  end
+
+  def test_course_to_html
+    @box.url = "https://connect.garmin.com:80/course/123456"
+    assert_equal "<iframe width='600' height='600' frameborder='0' src='https://connect.garmin.com/course/embed/123456'></iframe>", @box.to_html
+  end
+
+  def test_activity_to_html
+    @box.url = "https://connect.garmin.com:80/modern/activity/123456"
+    assert_equal "<iframe width='475' height='497' frameborder='0' src='https://connect.garmin.com/modern/activity/embed/123456'></iframe>", @box.to_html
+  end
 end
