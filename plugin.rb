dc8adf5329a2ee1fc6981ead74390de47e78450c
panbroda <panbroda@paga.ninja>
2016-08-03 14:26:29 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,20 +9,15 @@ module Onebox
   module Engine
     class GarminConnectOnebox
       include Engine
-      REGEX = /^https?:\/\/paga\.ninja(?::80)?\/(?:course|modern\/(?:course|activity))\/(?:embed\/)?([0-9]*)/
+      REGEX = /^https?:\/\/paga\.ninja(?::80)?\/lutim\/gallery(.*)/gm
       matches_regexp REGEX
 
       def id
         @url.match(REGEX)[1]
       end
 
-      def is_course?
-        @url.include? "course"
-      end
-
       def to_html
-        if is_course?
-          "<iframe width='600' height='600' frameborder='0' src='https://connect.garmin.com/course/embed/#{id}'></iframe>"
+          "<iframe width='600' height='600' frameborder='0' src='http://paga.ninja/lutim/gallery{id}'></iframe>"
        # else
       #    "<iframe width='475' height='497' frameborder='0' src='https://connect.garmin.com/modern/activity/embed/#{id}'></iframe>"
         end
