3603d20d68066372e64d9f1423caa922bcaf544e
panbroda <panbroda@paga.ninja>
2016-08-03 13:21:54 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,14 +2,14 @@
 # about: Garmin connect Onebox for embedding garmin courses and activities
 # version: 1.0
 # authors: Ewan McDougall
-
+# http://paga.ninja/lutim/gallery
 Onebox = Onebox
 
 module Onebox
   module Engine
     class GarminConnectOnebox
       include Engine
-      REGEX = /^https?:\/\/connect\.garmin\.com(?::80)?\/(?:course|modern\/(?:course|activity))\/(?:embed\/)?([0-9]*)/
+      REGEX = /^https?:\/\/paga\.ninja(?::80)?\/(?:course|modern\/(?:course|activity))\/(?:embed\/)?([0-9]*)/
       matches_regexp REGEX
 
       def id
@@ -23,8 +23,8 @@ module Onebox
       def to_html
         if is_course?
           "<iframe width='600' height='600' frameborder='0' src='https://connect.garmin.com/course/embed/#{id}'></iframe>"
-        else
-          "<iframe width='475' height='497' frameborder='0' src='https://connect.garmin.com/modern/activity/embed/#{id}'></iframe>"
+       # else
+      #    "<iframe width='475' height='497' frameborder='0' src='https://connect.garmin.com/modern/activity/embed/#{id}'></iframe>"
         end
       end
     end
